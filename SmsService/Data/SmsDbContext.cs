8e3f0bf2a653962c9c2e9a935d3a583f2ac12096
Veysel <71026333+veyselhim@users.noreply.github.com>
2021-06-19 02:59:56 +0300

    HomerWork 3

---
+++ SmsService/Data/SmsDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SmsService.Data
+{
+    public class SmsDbContext : IdentityDbContext
+    {
+        public SmsDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+
+    }
+}
---
+++ SmsService/JwtService.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using SmsService.Data;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmsService
+{
+    public class JwtService
+    {
+        private IConfiguration _configuration;
+        public JwtService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public string CreateAccessToken(User user)
+        {
+           
+            SymmetricSecurityKey securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+            SigningCredentials signingCredentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            JwtSecurityToken jwtSecurityToken = new JwtSecurityToken(issuer: _configuration["Jwt:Issuer"],
+                audience: _configuration["Jwt:Audience"] , signingCredentials: signingCredentials,
+                expires:DateTime.Now.AddHours(1));
+
+           
+
+            JwtSecurityTokenHandler jwtSecurityTokenHandler = new JwtSecurityTokenHandler();
+            return jwtSecurityTokenHandler.WriteToken(jwtSecurityToken);
+        }
+    }
+}
